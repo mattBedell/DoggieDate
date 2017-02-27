27c77c3ca6b36fe9c3e07fe489eeda7d4da79502
Matt Bedell <matt@bedell.co>
2017-02-27 17:39:00 -0500

    add schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,61 @@
+BEGIN;
+
+TRUNCATE members, dogs, d_attrs, dog_attr_refs RESTART IDENTITY CASCADE;
+
+INSERT INTO members(first, last, username, password, zip) VALUES
+('Matt', 'Bedell', 'mBedell', 'hash12345', '07030'),
+('Aaron', 'Conklin', 'aConklin', 'hash12345', '10010');
+
+INSERT INTO dogs (member_id, name, age, weight) VALUES
+(1, 'Tucker', 16, 20),
+(1, 'Luigi', 10, 10),
+(2, 'Frenchy', 1, 10);
+
+INSERT INTO d_attrs (attribute, attr_catagory) VALUES
+('chase', 'activities'),
+('fetch', 'activities'),
+('running', 'activities'),
+('walking', 'activities'),
+('biking', 'activities'),
+('hiking', 'activities'),
+('wrestling', 'activities'),
+('exploring', 'activities'),
+
+('mellow', 'temperament'),
+('shy', 'temperament'),
+('confident', 'temperament'),
+('fearful', 'temperament'),
+('frantic', 'temperament'),
+('high energy', 'temperament'),
+('low energy', 'temperament'),
+
+('tennis balls', 'interests'),
+('sticks', 'interests'),
+('bacon', 'interests'),
+('getting pet', 'interests'),
+('digging', 'interests'),
+('sniffing', 'interests'),
+('hanging out', 'interests');
+
+INSERT INTO dog_attr_refs (dog_id, attr_id) VALUES
+(1, 4),
+(1, 6),
+(1, 11),
+(1, 21),
+(1, 22),
+(2, 2),
+(2, 4),
+(2, 10),
+(2, 12),
+(2, 16),
+(2, 18),
+(2, 19),
+(3, 4),
+(3, 5),
+(3, 9),
+(3, 14),
+(3, 18),
+(3, 19),
+(3, 22);
+
+COMMIT;
