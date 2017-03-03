c456a47a49694760ca3a4e698b0e29600861b12e
Matt Bedell <matt@bedell.co>
2017-03-03 13:34:27 -0500

    remove img tags

--- application/src/components/App/Header/Header.jsx
+++ application/src/components/App/Header/Header.jsx
@@ -8,11 +8,6 @@ class Header extends Component {
         <img id='logo' src='./../../../application/assets/DoggieDateLogo.png' alt='DoggieDate' />
         <input placeholder='search users' />
         <div className='nav'>
-          <img className='icon' src='./../../../application/assets/icon-global.png' alt='Global' />
-          <img className='icon' src='./../../../application/assets/icon-nearby.png' alt='Nearby' />
-          <img className='icon' src='./../../../application/assets/icon-match.png' alt='Matches' />
-          <img className='icon' src='./../../../application/assets/icon-chat.png' alt='Chats' />
-          <img className='icon' src='./../../../application/assets/icon-profile.png' alt='Profile' />
         </div>
       </div>
     );
