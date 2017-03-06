3cf363ad7a51c1ded5caab7e1f5f4d35cd031308
Matt Bedell <matt@bedell.co>
2017-03-06 18:31:39 -0500

    add basic jwt verification

--- models/login.js
+++ models/login.js
@@ -1,5 +1,6 @@
 const db = require('./../lib/dbConnect');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 function createUser(req, res, next) {
 
@@ -7,11 +8,15 @@ function createUser(req, res, next) {
     name: 'create user',
     text: `INSERT INTO members(first, last, username, password, email, salt, zip) VALUES(
             $1, $2, $3, $4, $5, $6, $7)
-          RETURNING first, last, username, zip`,
+          RETURNING username`,
     values: [req.body.first, req.body.last, req.body.username, req.body.password, req.body.email, req.body.salt, req.body.zip]
   })
   .then((userData) => {
-    res.userData = userData;
+
+    let token = jwt.sign({
+      data: {user: userData.username}
+    }, process.env.TOKEN_SECRET, { expiresIn: '10s' });
+    console.log(token);
     next()
   })
   .catch((err) => {
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 const login = require('./../models/login.js');
+const jwt = require('jsonwebtoken');
 
 
 
 router.route('/checkuser')
   .post(login.checkUser)
 router.route('/create')
-  .post(login.createUser, (req, res, next) => { res.send(res.userData) })
+  .post(login.createUser, (req, res, next) => { res.send(res.token) })
+
+router.route('/checkToken')
+  .post((req, res, next) => {
+    jwt.verify(req.body.token, process.env.TOKEN_SECRET, (err, decoded) => {
+      console.log(decoded, err);
+    })
+  })
 
 
 module.exports = router;
