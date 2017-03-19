30de5ab72c8ebc975bf1fe2df8572c937995b4ec
Matt Bedell <matt@bedell.co>
2017-03-19 16:51:49 -0400

    add connect history to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
+const history = require('connect-history-api-fallback');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 require('dotenv').config();
@@ -24,7 +25,7 @@ app.use(bodyParser.json());
 app.use('/login', loginRoute);
 app.use('/api/users', usersRoute);
 
-
+app.use(history())
 if(isDev) {
   app.use(logger('dev'))
   app.use(require("webpack-dev-middleware")(compiler, {
