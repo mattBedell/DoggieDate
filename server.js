f7dc65ce45a31a83e0ffb1540b18985559e936f3
Matt Bedell <matt@bedell.co>
2017-03-22 20:44:59 -0400

    fix .env require

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const webpack = require('webpack');
 const history = require('connect-history-api-fallback');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+require('dotenv').config();
 const loginRoute = require('./routes/login.js');
 const usersRoute = require('./routes/users.js');
 const dogsRoute = require('./routes/dogs.js');
 
-const app = express();
-
+console.log(process.env.DB_HOST, process.env.DB_DATABASE)
 const isDev = process.env.NODE_ENV || true;
 const config = require(path.join(__dirname, '/webpack.config.js'));
 const compiler = webpack(config);
@@ -23,7 +24,6 @@ app.use('/api/dogs', dogsRoute);
 app.use(history());
 
 if (isDev) {
-  require('dotenv').config();
   app.use(logger('dev'));
   app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true, publicPath: config.output.publicPath,
