63f18108ef98e0970ac50406bde7b94f5a351918
Matt Bedell <matt@bedell.co>
2017-03-24 15:10:52 -0400

    add routes

--- application/src/components/App/App.jsx
+++ application/src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router'
+import { Route, Switch } from 'react-router'
 import Header from './../App/Header/Header.jsx';
 import Main from './../App/Main/Main.jsx';
 import SideBar from './../App/SideBar/SideBar.jsx';
 import Footer from  './../App/Footer/Footer.jsx';
 import styles from './App.css';
 import Global from './Global/Global.jsx'
-import MemberProfile from './MemberProfile/MemberProfile.jsx'
+import MemberProfile from './MemberProfile/MemberProfile.jsx';
+import MemberDogs from './MemberProfile/MemberDogs/MemberDogs.jsx';
 
 class App extends Component {
   render() {
@@ -14,8 +15,10 @@ class App extends Component {
       <div className='app'>
         <Header />
         <div className="center">
-          <Route path="/global" component={Global} />
-          <Route path="/profile:id" compnent={MemberProfile} />
+          <Switch>
+            <Route path="/dogs" component={MemberDogs} />
+            <Route path="/global" render={() => <Global />} />
+          </Switch>
           <SideBar />
         </div>
         <Footer />
