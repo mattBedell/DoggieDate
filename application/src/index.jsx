37edc8bf90da59ed574180f78b2f8387d84e5bd2
Matt Bedell <matt@bedell.co>
2017-03-23 23:33:23 -0400

    remove extra imports and linting to index jsx

--- application/src/index.jsx
+++ application/src/index.jsx
@@ -3,13 +3,12 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { store } from './store.js';
-import App from './components/App/App.jsx';
 import routes from './routes.jsx';
 
 
 localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hdHQiLCJpYXQiOjE0OTAwMzE2ODUsImV4cCI6MTUyMTU4OTI4NSwiaXNzIjoiRG9nZ2llIERhdGUifQ.K6OVH0Y2cEbLqDNLDtAuhxUhuaJ5ih_zQLYSw3O_IA0');
 
-const rootContainer = document.getElementById('root-container')
+const rootContainer = document.getElementById('root-container');
 
 const root = () => (
   <Router history={browserHistory} key={Math.random()}>
@@ -21,7 +20,7 @@ render(
   <Provider store={store} >
     {root()}
   </Provider>
-  , rootContainer)
+  , rootContainer);
 
 if (module.hot) {
   module.hot.accept();
