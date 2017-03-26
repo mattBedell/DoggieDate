7c73491554dac2b8ae4a37a55650c0f73d4804df
Matt Bedell <matt@bedell.co>
2017-03-26 16:49:09 -0400

    add dog buttons

--- application/src/components/App/MemberProfile/MemberDogs/MemberDogs.jsx
+++ application/src/components/App/MemberProfile/MemberDogs/MemberDogs.jsx
@@ -4,9 +4,30 @@ import { getDog } from './../../../../reducers/index';
 import './MemberDogs.css';
 import MemberDogsInfo from './MemberDogsInfo/MemberDogsInfo.jsx';
 
+const nextDog = ({ dogIds, match, history }, direction) => {
+  const dogInd = dogIds.indexOf(match.params.id);
+  switch (direction) {
+
+    case 'forward':
+    if (dogInd >= 0 && dogInd + 1 < dogIds.length) {
+      history.push(`/dog/${dogIds[dogInd+1]}`);
+    };
+    break;
+    case 'back':
+    if (dogInd >= 1) {
+      history.push(`/dog/${dogIds[dogInd-1]}`);
+    break;
+    };
+  };
+};
+
 const MemberDogs = (props) => {
   return (
     <div className='memberdogs'>
+      <div className="nav-view">
+        <div onClick={() => nextDog(props, 'back')}>{"<"}</div>
+        <div onClick={() => nextDog(props, 'forward')}>{">"}</div>
+      </div>
       <MemberDogsInfo {...props.profile} />
       <div className="dog-pic-box">
         <img src="" alt="dog pic" />
@@ -17,11 +38,12 @@ const MemberDogs = (props) => {
       </div>
     </div>
   );
-}
+};
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    profile: getDog(state, ownProps.match.params.id)
+    profile: getDog(state, ownProps.match.params.id),
+    dogIds: state.entities.dogs.allIds
   }
 }
 
---
+++ models/attributes.js
@@ -0,0 +1,15 @@
+const db = require('./../lib/dbConnect.js');
+
+function getAllAttributes(req, res, next) {
+  db.many({
+    name: 'get all atrributes from database',
+    text: 'SELECT * FROM d_attrs;'
+  })
+  .then((data) => {
+    res.data = data;
+    next()
+  }).catch((err) => {
+    console.log("--> Err getAllAttributes");
+    next(err);
+  })
+}
