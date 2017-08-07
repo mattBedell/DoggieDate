c0fb47848dd49c6b32fa369976eb0b9756d86206
Matt Bedell <matt@bedell.co>
2017-08-06 20:49:07 -0400

    add logging to server

--- api/server.js
+++ api/server.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs');
 require('dotenv');
 require('body-parser');
 
+const logger = require('./utils/logger');
 const routes = require('./routes/routes');
 
 const app = express();
 
+// ----- LOGGING >>>
+const accessStream = fs.createWriteStream('./logs/access.log', { flags: 'a' });
+const errStream = fs.createWriteStream('./logs/err.log', { flags: 'a' });
 
-app.use(morgan('dev', {
+app.use(morgan('combined', {
   skip: function(req, res) {
     return res.statusCode < 400;
   },
-  stream: process.stdout,
+  stream: errStream,
 }));
+app.use(morgan('combined', {
+  skip: function(req, res) {
+    return res.statusCode >= 400;
+  },
+  stream: accessStream,
+}));
+app.use(morgan('dev'));
+// ----- LOGGING />>>
 
 app.use(routes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`DOGGIE DATE API LISTENING ON PORT: ${PORT}`));
+app.listen(PORT, () => logger.debug(`DOGGIE DATE API LISTENING ON PORT: ${PORT}`));
 
